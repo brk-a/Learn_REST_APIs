3b3860feff122c08a10420d2096f4df2eb0c9481
brk-a <fnjakai@outlook.com>
2022-11-21 16:47:57 +0000

    set up `nodemon`: done. Server works correctly locally.

--- 0x01-learn_rest_api/nf-api/index.js
+++ 0x01-learn_rest_api/nf-api/index.js
@@ -6,4 +6,6 @@ const PORT = 5000
 
 app.use(bodyParser.json())
 
+app.get('/', (_, res) => res.send('Hello from Homepage!'))
+
 app.listen(PORT, () => console.log(`Server running on port: http://localhost${PORT}`))
