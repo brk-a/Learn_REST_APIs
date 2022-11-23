452ed4546b4b5948020678b519578a099d7dde09
brk-a <fnjakai@outlook.com>
2022-11-23 15:40:43 +0000

    `GET` using unique `id`: done

--- 0x01-learn_rest_api/nf-api/routes/users.js
+++ 0x01-learn_rest_api/nf-api/routes/users.js
@@ -7,56 +7,56 @@ const users = [
         lname: 'Matata',
         role: 'Founder',
         organisation: 'The GOAT Podcast',
-        id: uuid4()
+        id: '2056c58d-8672-48eb-8bd5-faa2daaa6a2a'
     },
     {
         fname: 'Dada',
         lname: 'Ng\'ombe',
         role: 'Founder',
         organisation: 'Chizi About Cheese',
-        id: uuid4()
+        id: 'bc775a9d-6fbd-4bb4-83e4-5d486576388b'
     },
     {
         fname: 'Kaka',
         lname: 'Sungura',
         role: 'Founder',
         organisation: 'Cunning Capital',
-        id: uuid4()
+        id: '4d355a95-1ca3-463a-9366-7263e98759cb'
     },
     {
         fname: 'Mzee',
         lname: 'Kobe',
         role: 'Founder',
         organisation: 'GoSlo Golf Tour',
-        id: uuid4()
+        id: 'f615546c-d247-4bb3-91a5-dd094dede293'
     },
     {
         fname: 'Paka',
         lname: 'the Cat',
         role: 'Founder',
         organisation: 'Catnip & Chill',
-        id: uuid4()
+        id: 'b965901a-63be-4f1d-8b20-e5c926e9a90f'
     },
     {
         fname: 'Kaka',
         lname: 'Mbwamwitu',
         role: 'Founder',
         organisation: 'Wild Good Boy',
-        id: uuid4()
+        id: '3adf13a8-1df3-42e6-b993-5cba21627fcf'
     },
     {
         fname: 'Kaka',
         lname: 'Mbweha',
         role: 'Founder',
         organisation: 'Fox News in the Henhouse',
-        id: uuid4()
+        id: '352481ef-3c88-459e-a94a-832c38285a13'
     },
     {
         fname: 'Kaka',
         lname: 'Dubu',
         role: 'Founder',
         organisation: 'Bear with Me',
-        id: uuid4()
+        id: 'ac3418fd-7378-4404-84a8-2e54b54f5768'
     }
 ]
 
@@ -76,9 +76,13 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
     const new_user = req.body
     users.push({...new_user, id: uuid4()})
-    res.send(`User id: ${id}, ${fname} ${lname}, has been added to database`)
+    res.send(`User id: ${new_user.id}, ${new_user.fname} ${new_user.lname}, has been added to database`)
 })
 
-router.get('/:id')
+router.get('/:id', (req, res) => {
+    const {id} = req.params
+    const foundUser = users.find((user) => user.id === id)
+    res.send(`GET request responded with id ${foundUser.id}`)
+})
 
 export default router
