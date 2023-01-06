3709a859b95d02da0dd6bd797af0123fca6a1ec7
brk-a <fnjakai@outlook.com>
2023-01-06 04:00:37 +0000

    routes: `query` and `param`

--- 0x02-learn_rest_api/1-first_endpoint/src/routes/person.js
+++ 0x02-learn_rest_api/1-first_endpoint/src/routes/person.js
@@ -1,8 +1,20 @@
 let express = require('express')
 let router = express.Router()
 
+// QueryString -> query property on request object
+// localhost:3000/person?key0=value0&key1=value1...&keyN=valueN
 router.get('/person', (req, res) => {
-    res.send('You have requested a person')
+    if (req.query.name){
+        res.send(`You requested person ${req.query.name}`)
+    } else {
+    res.send('You have requested no persons')
+    }
+})
+
+//params property on request object
+// localhost:3000/person/insert_param_here
+router.get('/person/:name', (req, res) => {
+    res.send(`You requested person ${req.params.name}`)
 })
 
 module.exports = router
