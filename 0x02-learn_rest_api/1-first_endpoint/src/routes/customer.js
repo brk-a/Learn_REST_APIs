2b539186a26c065972b5e9d89b12a2b0b06d6ad1
brk-a <fnjakai@outlook.com>
2023-01-09 10:13:09 +0000

    CRUD for `customer`: done

--- 0x02-learn_rest_api/1-first_endpoint/src/routes/customer.js
+++ 0x02-learn_rest_api/1-first_endpoint/src/routes/customer.js
@@ -3,6 +3,10 @@ let express = require('express')
 
 let router = express.Router()
 
+/**
+ * CRUD -> Create Read Update Delete
+ */
+
 //create a new customer
 //POST localhost:3000/customer
 router.post('/customer', (req, res) => {
@@ -20,6 +24,50 @@ router.post('/customer', (req, res) => {
             res.status(201).send(doc)
         })
         .catch(err => res.status(500).json(err))
+})
+
+//read data of a customer
+//GET localhost:3000/customer/:email
+router.get('/customer', (res, req) => {
+    if (!req.query.email) {
+        return res.status(400).send('Missing URL parameter: email')
+    }
+
+    CustomerModel.findOne({
+        email: req.query.email
+    })
+    .then(doc => res.json(doc))
+    .catch(err => res.status(500).json(err))
+})
+
+//update data of a customer
+//PUT localhost:3000/customer/:email
+router.put('/customer', (req, res) => {
+    if (!req.query.email) {
+        return res.sendStatus(400).send('Missing URL parameter: email')
+    }
+
+    CustomerModel.findOneAndUpdate({
+        email: req.query.email
+    }, req.body, {
+        new: true
+    })
+    .then(doc => res.json(doc))
+    .catch(err => res.status(500).json(err))
+})
+
+//delete data of a customer
+//DELETE localhost:3000/customer/:email
+router.delete('/customer', (req, res) => {
+    if (!req.query.email) {
+        res.status(400).send('Missing URL paramater: email')
+    }
+    
+    CustomerModel.findOneAndRemove({
+        email: req.query.email
+    })
+    .then(doc => res.json(doc))
+    .catch(err => res.status(500).json(err))
 })
 
 module.exports = router
