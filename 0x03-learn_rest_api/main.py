fb738c1204382248e8c185cc528a288538e983e3
brk-a <fnjakai@outlook.com>
2023-01-11 09:34:44 +0000

    implement a schema

--- 0x03-learn_rest_api/main.py
+++ 0x03-learn_rest_api/main.py
@@ -1,6 +1,28 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+# from box import Box #use dot notation to access values in a dictionary w/o using the class below
+from pydantic import BaseModel
+from typing import Optional
+
+
+# class AttributeDict(dict):
+#     """use dot notation to access values in a dictionary"""
+#     __getattr__ = dict.__getitem__
+#     __setattr__ = dict.__setitem__
+#     __delattr__ = dict.__delitem__
+
+
+class Post(BaseModel):
+    """ Schema for a post"""
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
 
 app = FastAPI()
+
 
 @app.get('/')
 async def root():
@@ -10,3 +32,17 @@ async def root():
 @app.get('/posts')
 def get_posts():
     return {"data": "This is a sample post"}
+
+
+@app.post('/createpost')
+def create_post(new_post: Post):
+    # new_post = AttributeDict(new_post) #IFF you use class AttributeDict
+    # new_post = Box(new_post) #IFF you use Box
+    print(new_post)
+    return {
+        "message": "Post created successfully",
+        "title": f"title: {new_post.title}", #instead of new_post['title']
+        "content": f"content: {new_post.content}", #instead of new_post['content']
+        "published": f"published: {new_post.published}",
+        "rating": f"rating: {new_post.rating}"
+    }
