3973293bf6fe68ee48551fed993a7f25847c6727
brk-a <fnjakai@outlook.com>
2023-01-19 12:42:55 +0000

    set up postgres access

--- 0x04-learn_rest_api/app/main.py
+++ 0x04-learn_rest_api/app/main.py
@@ -1,4 +1,29 @@
 import psycopg2
+from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+import os
 
-conn = psycopg2.connect("dbname=my_api user=postgres")
-cur = conn.cursor()
+
+class Post(BaseModel):
+    """class Post"""
+    title: str
+    content: str
+    published: bool = True
+
+app = FastAPI()
+
+if os.access("../.env", os.R_OK):
+    with open('.env') as f:
+        user_name = f[0]
+        password = f[1]
+
+try: 
+    conn = psycopg2.connect(host="localhost", database="fastapi", user=user_name, password=password)
+    cur = conn.cursor()
+except:
+    raise
+else:
+    pass
+finally:
+    pass
