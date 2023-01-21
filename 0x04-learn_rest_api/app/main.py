3418441211cc4da39a247b602908f2923abceba4
brk-a <fnjakai@outlook.com>
2023-01-21 03:36:51 +0000

    CRUD ops using postgres: parts C & R

--- 0x04-learn_rest_api/app/main.py
+++ 0x04-learn_rest_api/app/main.py
@@ -1,8 +1,10 @@
 import psycopg2
-from fastapi import FastAPI
+import os
+import time
+from fastapi import FastAPI, HTTPException, status
 from fastapi.params import Body
 from pydantic import BaseModel
-import os
+from psycopg2.extras import RealDictCursor
 
 
 class Post(BaseModel):
@@ -18,12 +20,43 @@ if os.access("../.env", os.R_OK):
         user_name = f[0]
         password = f[1]
 
-try: 
-    conn = psycopg2.connect(host="localhost", database="fastapi", user=user_name, password=password)
-    cur = conn.cursor()
-except:
-    raise
-else:
-    pass
-finally:
-    pass
+while True:
+    try: 
+        conn = psycopg2.connect(host="localhost", database="fastapi", user=user_name,
+            password=password, cursor_factory=RealDictCursor)
+        cur = conn.cursor()
+        print(f'Connection created successfully')
+        break
+    except Exception as err:
+        print(f'Connection was not created')
+        print(f'Error is... {err}')
+        time.sleep(2)
+
+
+@app.get("/posts")
+def get_posts():
+    """fetch all posts"""
+    cur.execute(""" SELECT * FROM posts """)
+    posts = cur.fetchall()
+    return {"data": posts}
+
+
+@app.post("/posts/{id}")
+def create_post(post: Post):
+    """create a post"""
+    cur.execute(""" INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING """,
+        (post.title, post.content, post.published))
+    new_post = cur.fetchone()
+    conn.commit()
+    return {"data": new_post}
+
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    """fetch one post"""
+    cur.execute(""" SELECT * FROM posts WHERE id = %s """, (str(id),))
+    post = cur.fetchone()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'post with id {id} was not found')
+    return {"data": post}
