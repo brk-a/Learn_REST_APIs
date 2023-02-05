02ab6688a84a6937321091858025472ab674c162
brk-a <fnjakai@outlook.com>
2023-02-05 05:26:08 +0000

    users must be logged in.

--- 0x05-learn_rest_api/app/oauth2.py
+++ 0x05-learn_rest_api/app/oauth2.py
@@ -8,8 +8,11 @@ from jose import JWTError, jwt
 from subprocess import Popen, PIPE
 from datetime import datetime, timedelta
 from .schemas import TokenData
+from .database import get_db
+from .models import User
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
@@ -51,9 +54,12 @@ def verify_access_token(token: str, credentials_exception):
     return token_data
 
 
-def get_current_user(token: str=Depends(oauth2_scheme)):
+def get_current_user(token: str=Depends(oauth2_scheme), db: Session=Depends(get_db)):
     """fetch the current user"""
     credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
         detail=f'Could not validate credentials', headers={"WWW-Authenticate": "Bearer" })
 
-    return verify_access_token(token, credentials_exception)
+    token_data = verify_access_token(token, credentials_exception)
+    user = db.query(User).filter(User.id == token_data.id).first()
+
+    return user 
