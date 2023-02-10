ec9e37e7f589fb9f3a0fa98fd5badf1c9474dd3b
brk-a <fnjakai@outlook.com>
2023-02-10 04:02:00 +0000

    simple votes/likes system, part 1: create the model

--- 0x05-learn_rest_api/app/models.py
+++ 0x05-learn_rest_api/app/models.py
@@ -34,3 +34,11 @@ class User(Base):
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'),
         nullable=False)
+
+
+class Vote(Base):
+    """model of the db containing vote data"""
+    __tablename__ = 'votes'
+
+    post_id = Column(Integer, ForeignKey("posts.id", ondelete="CASCADE"), primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE") , primary_key=True)
