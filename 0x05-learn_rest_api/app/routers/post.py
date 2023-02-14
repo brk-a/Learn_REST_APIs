736b633ce87eb32d5d90fc979af426de406f4b59
brk-a <fnjakai@outlook.com>
2023-02-14 15:28:01 +0000

    use joins to fetch posts and votes

--- 0x05-learn_rest_api/app/routers/post.py
+++ 0x05-learn_rest_api/app/routers/post.py
@@ -7,6 +7,7 @@ from .. import models, schemas, oauth2
 from ..database import get_db
 from sqlalchemy.orm import Session
 from typing import List, Optional
+from sqlalchemy import func
 
 router = APIRouter(prefix="/posts", tags=['Posts'])
 
@@ -33,6 +34,7 @@ def get_posts(db: Session=Depends(get_db), get_current_user : int=Depends(oauth2
     limit: int=10, skip: int=0, search: Optional[str]=""):
     """fetch all posts"""
     posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+    results = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(models.Vote, models.Vote.post_id==models.Post.id, isouter=True).group_by(models.Post.id).all()
     return posts
 
 
