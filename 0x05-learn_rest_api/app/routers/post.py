4270bce841a42c0d76681fcdf0eba90ae75f51e2
brk-a <fnjakai@outlook.com>
2023-02-16 03:47:02 +0000

    ..

--- 0x05-learn_rest_api/app/routers/post.py
+++ 0x05-learn_rest_api/app/routers/post.py
@@ -33,15 +33,15 @@ def create_post(post: schemas.PostCreate, db: Session=Depends(get_db), get_curre
 def get_posts(db: Session=Depends(get_db), get_current_user : int=Depends(oauth2.get_current_user),
     limit: int=10, skip: int=0, search: Optional[str]=""):
     """fetch all posts"""
-    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
-    results = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(models.Vote, models.Vote.post_id==models.Post.id, isouter=True).group_by(models.Post.id).all()
-    return results
+    # posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+    posts = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(models.Vote, models.Vote.post_id==models.Post.id, isouter=True).group_by(models.Post.id).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+    return posts
 
 
-@router.get("/{id}", response_model=schemas.PostResponseBase)
+@router.get("/{id}", response_model=schemas.PostResponseJoinVote)
 def get_post(id: int, db: Session=Depends(get_db), get_current_user : int=Depends(oauth2.get_current_user)):
     """fetch one post"""
-    post = db.query(models.Post).filter(models.Post.id == id).first()
+    post = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(models.Vote, models.Vote.post_id==models.Post.id, isouter=True).group_by(models.Post.id).filter(models.Post.id == id).first()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
             detail=f'post with id {id} was not found')
